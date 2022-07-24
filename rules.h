d7b00a3a4f1769ccd8264ef32645a6dafa6bba63
speediegq <speedie.alt@gmail.com>
2022-07-24 20:49:07 +0200

    small aesthetics tweak

--- rules.h
+++ rules.h
@@ -4,6 +4,7 @@
  *
  * - tags mask: Disable tags where the CLASS, INSTANCE or TITLE will spawn. (X << Y)
  * - isfloating: Choose whether a CLASS, INSTANCE or TITLE will spawn as floating. (1/0)
+ * - canfocus: Whether or not the CLASS, INSTANCE or TITLE can be focused or not. (1/0)
  * - isterminal: Whether or not the CLASS, INSTANCE or TITLE is a terminal or not (1/0)
  * - noswallow: Whether or not the CLASS, INSTANCE or TITLE gets swallowed by a terminal or not (1/0)
  * - centerfirst: Whether or not the CLASS, INSTANCE or TITLE gets centered if it's the first window on a tag (1/0)
@@ -14,25 +15,25 @@
  * 
  ***************************************************************/
 static const Rule rules[]                     = {
-    	/* class                instance    title                             tags mask    isfloating  canfocus      isterminal   noswallow   centerfirst    monitor      ignoretransient    scratch key */
-		{ TERMINAL_CLASS,       NULL,       NULL,                             0,           0,          1,            1,           0,          0,             -1,          0,                 0  },
-        { TERMINAL_CLASS,       NULL,       "sxiv",                           0,           0,          1,            0,           1,          0,             -1,          0,                 0  },
-        { TERMINAL_CLASS,       NULL,       FILEMANAGER_CLASS,                0,           0,          1,            1,           0,          0,             -1,          0,                 0  },
-        { PDF_CLASS,            NULL,       NULL,                             0,           0,          1,            0,           0,          0,             -1,          0,                 0  },
-        { TERMINAL_CLASS,       NULL,       EDITOR,                           0,           0,          1,            0,           0,          0,             -1,          0,                 0  },
-        { TERMINAL_CLASS,       NULL,       MUSIC,                            0,           0,          1,            0,           1,          0,             -1,          0,                 0  },
-        { TERMINAL_CLASS,       NULL,       "dwm",                            0,           0,          1,            0,           1,          0,             -1,          0,                 0  },
-        { TERMINAL_CLASS,       NULL,       MIXER,                            0,		   1,		   1,            0,           1,          1,             -1,          0,                 0  },
-	    { BROWSER_CLASS,        NULL,       NULL,                             0,           0,          1,            0,           1,          0,             -1,          0,                 0  },
-		{ "dolphin-emu",        NULL,       NULL,                             0,           1,          1,            1,           0,          1,             -1,          0,                 0  },
-        { "mpv",                NULL,       NULL,                             0,           0,          1,            0,           0,          0,             -1,          0,                 0  },
-        { "tabbed",             NULL,       NULL,                             0,           0,          1,            0,           0,          0,             -1,          0,                 0  },
-        { "trayer",             NULL,       NULL,                             0,           1,          0,            0,           1,          0,             -1,          0,                 0  },
-		{ NULL,                 NULL,       "CustomizeMii 3.11 by Leathl",    0,           1,          1,            0,           1,          1,             -1,          0,                 0  },
-	    { NULL,                 NULL,       "Picture-in-Picture",             0,           1,          1,            0,           1,          0,             -1,          0,                 0  },
-	    { NULL,                 NULL,       "About GNU IceCat",               0,           1,          1,            0,           1,          0,             -1,          0,                 0  },
-	    { NULL,                 NULL,       "About LibreWolf",                0,           1,          1,            0,           1,          0,             -1,          0,                 0  },
-	    { NULL,                 NULL,       "About Mozilla Firefox",          0,           1,          1,            0,           1,          0,             -1,          0,                 0  },
-		{ NULL,                 NULL,       "scratchpad",                     0,                       0,            0,                                      -1,                            's' },
+    	/* class                instance    title                             tags mask isfloating canfocus isterminal noswallow centerfirst monitor ignoretransient scratch key */
+		{ TERMINAL_CLASS,       NULL,       NULL,                             0,        0,         1,       1,         0,        0,          -1,     0,              0  },
+        { TERMINAL_CLASS,       NULL,       "sxiv",                           0,        0,         1,       0,         1,        0,          -1,     0,              0  },
+        { TERMINAL_CLASS,       NULL,       FILEMANAGER_CLASS,                0,        0,         1,       1,         0,        0,          -1,     0,              0  },
+        { PDF_CLASS,            NULL,       NULL,                             0,        0,         1,       0,         0,        0,          -1,     0,              0  },
+        { TERMINAL_CLASS,       NULL,       EDITOR,                           0,        0,         1,       0,         0,        0,          -1,     0,              0  },
+        { TERMINAL_CLASS,       NULL,       MUSIC,                            0,        0,         1,       0,         1,        0,          -1,     0,              0  },
+        { TERMINAL_CLASS,       NULL,       "dwm",                            0,        0,         1,       0,         1,        0,          -1,     0,              0  },
+        { TERMINAL_CLASS,       NULL,       MIXER,                            0,	    1,		   1,       0,         1,        1,          -1,     0,              0  },
+	    { BROWSER_CLASS,        NULL,       NULL,                             0,        0,         1,       0,         1,        0,          -1,     0,              0  },
+		{ "dolphin-emu",        NULL,       NULL,                             0,        1,         1,       1,         0,        1,          -1,     0,              0  },
+        { "mpv",                NULL,       NULL,                             0,        0,         1,       0,         0,        0,          -1,     0,              0  },
+        { "tabbed",             NULL,       NULL,                             0,        0,         1,       0,         0,        0,          -1,     0,              0  },
+        { "trayer",             NULL,       NULL,                             0,        1,         0,       0,         1,        0,          -1,     0,              0  },
+		{ NULL,                 NULL,       "CustomizeMii 3.11 by Leathl",    0,        1,         1,       0,         1,        1,          -1,     0,              0  },
+	    { NULL,                 NULL,       "Picture-in-Picture",             0,        1,         1,       0,         1,        0,          -1,     0,              0  },
+	    { NULL,                 NULL,       "About GNU IceCat",               0,        1,         1,       0,         1,        0,          -1,     0,              0  },
+	    { NULL,                 NULL,       "About LibreWolf",                0,        1,         1,       0,         1,        0,          -1,     0,              0  },
+	    { NULL,                 NULL,       "About Mozilla Firefox",          0,        1,         1,       0,         1,        0,          -1,     0,              0  },
+		{ NULL,                 NULL,       "scratchpad",                     0,                   0,       0,                               -1,                    's' },
 };
 
